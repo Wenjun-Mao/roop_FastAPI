5669296c3b01f6f4ae6d790356a1db4992844a12
Wenjun Mao <mkof616@gmail.com>
2023-07-18 12:33:59 -0400

    Minor fix

--- api_run_roop.py
+++ api_run_roop.py
@@ -7,7 +7,7 @@ def change_directory() -> None:
 
     parent_dir = os.path.dirname(cwd)
     print(f"Parent directory: {parent_dir}")
-    new_dir = os.path.join(parent_dir, "roop")
+    new_dir = os.path.join(parent_dir, "roop_backend")
 
     if os.path.exists(new_dir):
         os.chdir(new_dir)
@@ -18,9 +18,6 @@ def change_directory() -> None:
     print(f"New working directory: {cwd}")  
 
 change_directory()
-
-from fastapi import FastAPI
-from pydantic import BaseModel
 
 from roop.core import *
 
@@ -37,8 +34,67 @@ roop.globals.execution_providers = decode_execution_providers(["cuda"])
 roop.globals.execution_threads = suggest_execution_threads()
 
 
+# Refactor some functions from roop.core
+def update_status(message: str, scope: str = 'ROOP.CORE') -> None:
+    print(f'[{scope}] {message}')
+    print(message)
+    print(scope)
+    if message == 'No face in source path detected.' and scope == 'ROOP.FACE-SWAPPER':
+        print('WOW WOW WOW')
 
 
+def start() -> None:
+    for frame_processor in get_frame_processors_modules(roop.globals.frame_processors):
+        if not frame_processor.pre_start():
+            return
+    # process image to image
+    if has_image_extension(roop.globals.target_path):
+        shutil.copy2(roop.globals.target_path, roop.globals.output_path)
+        for frame_processor in get_frame_processors_modules(roop.globals.frame_processors):
+            update_status('Progressing...', frame_processor.NAME)
+            frame_processor.process_image(roop.globals.source_path, roop.globals.output_path, roop.globals.output_path)
+            frame_processor.post_process()
+            release_resources()
+        if is_image(roop.globals.target_path):
+            update_status('Processing to image succeed!')
+        else:
+            update_status('Processing to image failed!')
+        return
+    # process image to videos
+    update_status('Creating temp resources...')
+    create_temp(roop.globals.target_path)
+    update_status('Extracting frames...')
+    extract_frames(roop.globals.target_path)
+    temp_frame_paths = get_temp_frame_paths(roop.globals.target_path)
+    for frame_processor in get_frame_processors_modules(roop.globals.frame_processors):
+        update_status('Progressing...', frame_processor.NAME)
+        frame_processor.process_video(roop.globals.source_path, temp_frame_paths)
+        frame_processor.post_process()
+        release_resources()
+    # handles fps
+    if roop.globals.keep_fps:
+        update_status('Detecting fps...')
+        fps = detect_fps(roop.globals.target_path)
+        update_status(f'Creating video with {fps} fps...')
+        create_video(roop.globals.target_path, fps)
+    else:
+        update_status('Creating video with 30.0 fps...')
+        create_video(roop.globals.target_path)
+    # handle audio
+    if roop.globals.keep_audio:
+        if roop.globals.keep_fps:
+            update_status('Restoring audio...')
+        else:
+            update_status('Restoring audio might cause issues as fps are not kept...')
+        restore_audio(roop.globals.target_path, roop.globals.output_path)
+    else:
+        move_temp(roop.globals.target_path, roop.globals.output_path)
+    # clean and validate
+    clean_temp(roop.globals.target_path)
+    if is_video(roop.globals.target_path):
+        update_status('Processing to video succeed!')
+    else:
+        update_status('Processing to video failed!')
 
 def run(**kwargs) -> None:
     if not pre_check():
@@ -49,14 +105,33 @@ def run(**kwargs) -> None:
             print(f'pre-check failed: {frame_processor.__name__}')
             return
     limit_resources()
-    print('headless mode')
     start()
 
 
 def release_resources() -> None:
-    print('--------releasing resources--------')
     torch.cuda.empty_cache()
 
+
+def swap_pre_start() -> bool:
+    print('------------------New swap_pre_start------------------')
+    if not is_image(roop.globals.source_path):
+        update_status('Select an image for source path.', NAME)
+        return False
+    elif not get_one_face(cv2.imread(roop.globals.source_path)):
+        update_status('No face in source path detected.', NAME)
+        return False
+    if not is_image(roop.globals.target_path) and not is_video(roop.globals.target_path):
+        update_status('Select an image or video for target path.', NAME)
+        return False
+    return True
+
+roop.core.update_status = update_status
+roop.core.release_resources = release_resources
+import roop.processors.frame.face_swapper
+roop.processors.frame.face_swapper.pre_start = swap_pre_start
+
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 class Body(BaseModel):
     source_path: str
