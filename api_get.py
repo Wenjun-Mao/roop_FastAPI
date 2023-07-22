1cac47ec00ef59f8c30f3319cf44aceb6dda7c34
Wenjun Mao <mkof616@gmail.com>
2023-07-21 20:57:53 -0400

    remove returning default file in get

--- api_get.py
+++ api_get.py
@@ -21,28 +21,19 @@ async def general_exception_handler(request, exc):
     logger.error(f"An error occurred: {exc}")
     return JSONResponse(
         status_code=500,
-        content={"message": "An error occurred. Please try again later."},
+        content={"message": "Error. Please try again later."},
     )
+
 
 @app.get("/download_video/{date}/{video_name}")
 async def download_video(date: str, video_name: str):
-    try:
-        file_path = f"{media_path}/api_outgoing/video/{date}/{video_name}"
-        logger.info(f"Downloading video: {file_path}")
-        return FileResponse(file_path)
-    except FileNotFoundError:
-        logger.info(f"Video not found, returning default video")
-        default_video_file = f"{media_path}/api_outgoing/video/{default_video_path}"
-        return FileResponse(default_video_file)
+    file_path = f"{media_path}/api_outgoing/video/{date}/{video_name}"
+    logger.info(f"Downloading video: {file_path}")
+    return FileResponse(file_path)
 
 
 @app.get("/download_pic/{date}/{pic_name}")
 async def download_pic(date: str, pic_name: str):
-    try:
-        file_path = f"{media_path}/api_outgoing/pic/{date}/{pic_name}"
-        logger.info(f"Downloading picture: {file_path}")
-        return FileResponse(file_path)
-    except FileNotFoundError:
-        logger.info(f"Picture not found, returning default picture")
-        default_pic_file = f"{media_path}/api_outgoing/pic/{default_picture_path}"
-        return FileResponse(default_pic_file)
+    file_path = f"{media_path}/api_outgoing/pic/{date}/{pic_name}"
+    logger.info(f"Downloading picture: {file_path}")
+    return FileResponse(file_path)
